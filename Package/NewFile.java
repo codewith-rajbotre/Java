baf7dbc7871243702f8f2ea4a28f25f9b3b5e720
Raj Botre <rajbotre2@gmail.com>
2025-03-12 21:19:13 +0530

    Package in Java

---
+++ Package/NewFile.java
@@ -0,0 +1,10 @@
+class Hello{
+    public void pack(){
+        System.out.println("In Own Package");
+    }
+}
+public class NewFile{
+    public static void main(String[] args) {
+        System.out.println("Package");
+    }
+}
