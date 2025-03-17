65956b6ceb563647f73016d12da0d59cc52f2711
Raj Botre <rajbotre2@gmail.com>
2025-03-17 22:33:54 +0530

    Code

---
+++ hello.java
@@ -0,0 +1,15 @@
+public class hello{
+    public static void main (String[]args)
+  {
+    int number = 22;
+    if((number&1)==1){
+        System.out.println("Odd");
+    }
+    else{
+        System.out.println("Even");
+    }
+  }
+
+
+
+}
