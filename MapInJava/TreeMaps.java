bfb161899ae1ed86ef8d27315edb8d2f251ba792
Raj Botre <rajbotre2@gmail.com>
2025-04-03 10:47:57 +0530

    TreeMap In Java

---
+++ MapInJava/TreeMaps.java
@@ -0,0 +1,28 @@
+
+import java.util.Iterator;
+import java.util.TreeMap;
+class TreeMaps {
+
+    public static void main(String[] args) {
+        System.out.println("Tree Map");
+        TreeMap<Integer, Integer> m = new TreeMap<>();
+        m.put(3, 11);
+        m.put(66, 56);
+        m.put(4, 54);
+        System.out.println(m);
+        // for (Integer key : m.keySet()) {
+        //     System.out.println(key + " -> " + m.get(key));
+        // }
+
+        // for (Integer value : m.values()) {
+        //     System.out.println(value);
+        // }
+        Iterator<Integer> keyIterator = m.keySet().iterator();
+        while(keyIterator.hasNext()) {
+            Integer key = keyIterator.next();
+            System.out.println(key + " -> " + m.get(key));
+        }
+
+    }
+
+}
