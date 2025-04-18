54ddfeafda7081d801940dd4d494376f81ebcfc9
Raj Botre <rajbotre2@gmail.com>
2025-04-18 16:29:08 +0530

    Tree in Java

---
+++ BinaryTree/TreeNode.java
@@ -0,0 +1,10 @@
+public class TreeNode{
+    public void TreeNode(){
+        int val ;
+        TreeNode left;
+        TreeNode right;
+    }
+    public static void main(String[] args) {
+        System.out.println("TreeNode");
+    }
+}
