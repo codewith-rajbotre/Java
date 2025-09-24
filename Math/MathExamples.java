e9e3de6f67adeb49744251dc78c50e44810b7e94
Raj Botre <rajbotre2@gmail.com>
2025-09-24 13:05:59 +0530

    Math - Final Class with Static methods

---
+++ Math/MathExamples.java
@@ -0,0 +1,16 @@
+public class MathExamples{
+    public static void main(String[] args) {
+        int num1 = 89;
+        int num2 = 32;
+        // Math.max(num1, num2)
+        System.out.println(Math.max(num1, num2));
+        // Math.round()
+        double num3 = 34.2;
+        System.out.println(Math.round(num3));
+
+        System.out.println(Math.floor(num3));
+        int example = -323;
+        System.out.println(Math.abs(example));
+    
+    }
+}
