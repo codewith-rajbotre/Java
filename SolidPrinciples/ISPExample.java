4581055e241ade55d68fc40b0d4524e6d4b44e8d
Raj Botre <rajbotre2@gmail.com>
2025-11-05 22:50:41 +0530

    Solid Principle : ISP Principle

---
+++ SolidPrinciples/ISPExample.java
@@ -0,0 +1,30 @@
+interface Workable {
+    void work();
+}
+
+interface Eatable {
+    void eat();
+}
+
+class Human implements Workable, Eatable {
+    public void work() {
+        System.out.println("Human working");
+    }
+
+    public void eat() {
+        System.out.println("Human eating");
+    }
+}
+
+class Robot implements Workable {
+    public void work() {
+        System.out.println("Robot working");
+    }
+}
+
+public class ISPExample {
+    public static void main(String[] args) {
+        Workable worker = new Robot();
+        worker.work();
+    }
+}
